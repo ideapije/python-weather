ce64f7f510cee6b9c9af099d1dc65baf4344f9a4
ifirmawan <firmawaneiwan@gmail.com>
2023-04-02 06:11:36 +0700

    add prefix_url and return data type

--- cuaca.py
+++ cuaca.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class Cuaca:
     def __init__(self, local_path):
         self.local_path = local_path
@@ -8,14 +9,15 @@ class Cuaca:
             "DIYogyakarta",
             "JawaBarat",
             "Bali",
-            "KalimantanSelatan"
+            "KalimantanSelatan",
         ]
-        self.wilayah_url = "https://ibnux.github.io/BMKG-importer/cuaca/wilayah.json"
+        self.prefix_url = "https://ibnux.github.io/BMKG-importer"
+        self.wilayah_url = f"{self.prefix_url}/cuaca/wilayah.json"
         self.wilayah_data = []
         self.wilayah_id_map = {}
         self.weather_data = []
-    
-    def get_wilayah_data(self):
+
+    def get_wilayah_data(self) -> dict:
         response = requests.get(self.wilayah_url)
         self.wilayah_data = response.json()
         for wilayah in self.wilayah_data:
@@ -23,3 +25,8 @@ class Cuaca:
             if key[0] in self.provinces:
                 self.wilayah_id_map[key] = wilayah["id"]
         return self.wilayah_id_map
+
+    def get_cuaca_by_id(self, id: int) -> list:
+        response = requests.get(f"{self.prefix_url}/cuaca/{id}.json")
+        self.weather_data = response.json()
+        return self.weather_data
